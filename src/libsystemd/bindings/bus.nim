15a57616aed3bb3b7154bbc4a76a6e0ec7d95d24
Trayambak Rai <xtrayambak@disroot.org>
2025-04-25 20:05:09 +0530

    add: bindings: more sd-bus progress

--- src/libsystemd/bindings/bus.nim
+++ src/libsystemd/bindings/bus.nim
@@ -182,6 +182,199 @@ proc sd_bus_get_event*(bus: ptr sd_bus): ptr sd_event
 
 proc sd_bus_get_n_queued_read*(bus: ptr sd_bus, ret: ptr uint64): int32
 proc sd_bus_get_n_queued_write*(bus: ptr sd_bus, ret: ptr uint64): int32
+
+proc sd_bus_set_method_call_timeout*(bus: ptr sd_bus, usec: uint64): int32
+proc sd_bus_get_method_call_timeout*(bus: ptr sd_bus, ret: ptr uint64): int32
+
+proc sd_bus_pending_method_calls*(bus: ptr sd_bus): int32
+
+proc sd_bus_slot_ref*(slot: ptr sd_bus_slot): ptr sd_bus_slot
+proc sd_bus_slot_unref*(slot: ptr sd_bus_slot): ptr sd_bus_slot
+
+proc sd_bus_slot_get_bus*(slot: ptr sd_bus_slot): ptr sd_bus
+proc sd_bus_slot_set_description*(slot: ptr sd_bus_slot, description: cstring): int32
+proc sd_bus_slot_get_description*(
+  slot: ptr sd_bus_slot, description: ptr cstring
+): int32
+
+proc sd_bus_slot_get_floating*(slot: ptr sd_bus_slot): int32
+proc sd_bus_slot_set_floating*(slot: ptr sd_bus_slot, b: int32): int32
+
+proc sd_bus_slot_get_current_message*(slot: ptr sd_bus_slot): ptr sd_bus_message
+proc sd_bus_slot_get_current_userdata*(slot: ptr sd_bus_slot): pointer
+
+proc sd_bus_message_new*(
+  bus: ptr sd_bus, message: ptr ptr sd_bus_message, typ: uint8
+): int32
+
+proc sd_bus_message_new_signal*(
+  bus: ptr sd_bus, m: ptr ptr sd_bus_message, path, iface, member: cstring
+): int32
+
+proc sd_bus_message_new_signal_to*(
+  bus: ptr sd_bus, m: ptr ptr sd_bus_message, destination, path, iface, member: cstring
+): int32
+
+proc sd_bus_message_new_method_call*(
+  bus: ptr sd_bus, m: ptr ptr sd_bus_message, dest, path, iface, member: cstring
+): int32
+
+proc sd_bus_message_new_method_return*(
+  call: ptr sd_bus_message, m: ptr ptr sd_bus_message
+): int32
+
+proc sd_bus_message_new_method_error*(
+  call: ptr sd_bus_message, m: ptr ptr sd_bus_message, e: ptr sd_bus_error
+): int32
+
+proc sd_bus_message_new_method_errno*(
+  call: ptr sd_bus_message, m: ptr ptr sd_bus_message, error: int32, e: ptr sd_bus_error
+): int32
+
+proc sd_bus_message_ref*(m: ptr sd_bus_message): ptr sd_bus_message
+proc sd_bus_message_unref*(m: ptr sd_bus_message): ptr sd_bus_message
+
+proc sd_bus_message_seal*(
+  m: ptr sd_bus_message, cookie: uint64, timeout_usec: uint64
+): int32
+
+proc sd_bus_message_get_type*(m: ptr sd_bus_message, typ: ptr uint8): int32
+proc sd_bus_message_get_cookie*(m: ptr sd_bus_message, cookie: ptr uint64): int32
+proc sd_bus_message_get_reply_cookie*(m: ptr sd_bus_message, cookie: ptr uint64): int32
+proc sd_bus_message_get_priority*(
+  m: ptr sd_bus_message, priority: ptr int64
+): int32 {.deprecated.}
+
+proc sd_bus_message_get_expect_reply*(m: ptr sd_bus_message): int32
+proc sd_bus_message_get_auto_start*(m: ptr sd_bus_message): int32
+proc sd_bus_message_get_allow_interactive_authorization*(m: ptr sd_bus_message): int32
+
+proc sd_bus_message_get_signature*(m: ptr sd_bus_message, complete: int32): cstring
+proc sd_bus_message_get_path*(m: ptr sd_bus_message): cstring
+proc sd_bus_message_get_interface*(m: ptr sd_bus_message): cstring
+proc sd_bus_message_get_member*(m: ptr sd_bus_message): cstring
+proc sd_bus_message_get_destination*(m: ptr sd_bus_message): cstring
+proc sd_bus_message_get_sender*(m: ptr sd_bus_message): cstring
+proc sd_bus_message_get_error*(m: ptr sd_bus_message): ptr sd_bus_error
+proc sd_bus_message_get_errno*(m: ptr sd_bus_message): int32
+
+proc sd_bus_message_get_monotonic_usec*(m: ptr sd_bus_message, usec: ptr uint64): int32
+proc sd_bus_message_get_realtime_usec*(m: ptr sd_bus_message, usec: ptr uint64): int32
+proc sd_bus_message_get_seqnum*(m: ptr sd_bus_message, seqnum: ptr uint64): int32
+
+proc sd_bus_message_get_bus*(m: ptr sd_bus_message): ptr sd_bus
+proc sd_bus_message_get_creds*(m: ptr sd_bus_message): ptr sd_bus_creds
+
+proc sd_bus_message_is_signal*(m: ptr sd_bus_message, iface, member: cstring): int32
+proc sd_bus_message_is_method_call*(
+  m: ptr sd_bus_message, iface, member: cstring
+): int32
+
+proc sd_bus_message_is_method_error*(m: ptr sd_bus_message, name: cstring): int32
+proc sd_bus_message_is_empty*(m: ptr sd_bus_message): int32
+proc sd_bus_message_has_signature*(m: ptr sd_bus_message, signature: cstring): int32
+
+proc sd_bus_message_set_expect_reply*(m: ptr sd_bus_message, b: int32): int32
+proc sd_bus_message_set_auto_start*(m: ptr sd_bus_message, b: int32): int32
+proc sd_bus_message_set_allow_interactive_authorization*(
+  m: ptr sd_bus_message, b: int32
+): int32
+
+proc sd_bus_message_set_destination*(m: ptr sd_bus_message, destination: cstring): int32
+proc sd_bus_message_set_sender*(m: ptr sd_bus_message, sender: cstring): int32
+proc sd_bus_message_set_priority*(
+  m: ptr sd_bus_message, priority: int64
+): int32 {.deprecated.}
+
+proc sd_bus_message_append_basic*(m: ptr sd_bus_message, typ: uint8, p: pointer): int32
+proc sd_bus_message_append_array*(
+  m: ptr sd_bus_message, typ: uint8, p: pointer, size: uint64
+): int32
+
+proc sd_bus_message_append_array_space*(
+  m: ptr sd_bus_message, typ: uint8, size: uint64, p: ptr pointer
+): int32
+
+proc sd_bus_message_append_array_iovec*(
+  m: ptr sd_bus_message, typ: uint8, iov: ptr UncheckedArray[IOVec], n: uint32
+): int32
+
+proc sd_bus_message_append_array_memfd*(
+  m: ptr sd_bus_message, typ: uint8, memfd: cint, offset: uint64, size: uint64
+): int32
+
+proc sd_bus_message_append_string_space*(
+  m: ptr sd_bus_message, size: uint64, s: ptr cstring
+): int32
+
+proc sd_bus_message_append_string_iovec*(
+  m: ptr sd_bus_message, iov: ptr UncheckedArray[IOVec], n: uint32
+): int32
+
+proc sd_bus_message_append_string_memfd*(
+  m: ptr sd_bus_message, memfd: cint, offset, size: uint64
+): int32
+
+proc sd_bus_message_append_strv*(m: ptr sd_bus_message, l: ptr cstring): int32
+proc sd_bus_message_open_container*(
+  m: ptr sd_bus_message, typ: uint8, contents: ptr UncheckedArray[cstring]
+): int32
+
+proc sd_bus_message_close_container*(m: ptr sd_bus_message): int32
+proc sd_bus_message_copy*(
+  m: ptr sd_bus_message, source: ptr sd_bus_message, all: int32
+): int32
+
+proc sd_bus_message_read_basic*(m: ptr sd_bus_message, typ: uint8, p: pointer): int32
+proc sd_bus_message_read_array*(
+  m: ptr sd_bus_message, typ: uint8, p: var pointer, size: ptr uint64
+): int32
+
+proc sd_bus_message_read_strv*(m: ptr sd_bus_message, l: ptr ptr cstring): int32
+proc sd_bus_message_read_strv_extend*(m: ptr sd_bus_message, l: ptr ptr cstring): int32
+proc sd_bus_message_skip*(
+  m: ptr sd_bus_message, types: ptr UncheckedArray[cstring]
+): int32
+
+proc sd_bus_message_enter_container*(
+  m: ptr sd_bus_message, typ: uint8, contents: ptr UncheckedArray[cstring]
+): int32
+
+proc sd_bus_message_exit_container*(m: ptr sd_bus_message): int32
+proc sd_bus_message_peek_type*(
+  m: ptr sd_bus_message, typ: ptr uint8, contents: ptr UncheckedArray[cstring]
+): int32
+
+proc sd_bus_message_verify_type*(
+  m: ptr sd_bus_message, typ: uint8, contents: ptr UncheckedArray[char]
+): int32
+
+proc sd_bus_message_at_end*(m: ptr sd_bus_message, complete: int32): int32
+proc sd_bus_message_rewind*(m: ptr sd_bus_message, complete: int32): int32
+proc sd_bus_message_sensitive*(m: ptr sd_bus_message): int32
+
+proc sd_bus_message_dump*(m: ptr sd_bus_message, f: File, flags: uint64): int32
+
+proc sd_bus_get_unique_name*(
+  bus: ptr sd_bus, unique: ptr UncheckedArray[cstring]
+): int32
+
+proc sd_bus_request_name*(bus: ptr sd_bus, name: cstring, flags: uint64): int32
+proc sd_bus_release_name*(bus: ptr sd_bus, name: cstring): int32
+proc sd_bus_list_names*(
+  bus: ptr sd_bus,
+  acquired: ptr ptr UncheckedArray[cstring],
+  activatable: ptr ptr UncheckedArray[cstring],
+): int32
+
+proc sd_bus_get_name_creds*(
+  bus: ptr sd_bus, name: cstring, mask: uint64, creds: ptr ptr sd_bus_creds
+): int32
+
+proc sd_bus_get_name_machine_id*(
+  bus: ptr sd_bus, name: cstring, machine: sd_id128
+): int32
+
 {.pop.}
 
 {.pop.}
